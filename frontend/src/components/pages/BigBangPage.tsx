fb851d2b958a9e84e4a632d563cb6bd3802361a3
Petdra75 <Petru.Draghiceanu@softwire.com>
2026-07-17 12:37:56 +0300

    completed website structure page

---
+++ frontend/src/components/pages/BigBangPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BigBangPage = () => {
+  return (
+    <div>BigBangPage</div>
+  )
+}
---
+++ frontend/src/components/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div style={{color:"white"}}>Sal</div>
+  )
+}
+
+export default HomePage
